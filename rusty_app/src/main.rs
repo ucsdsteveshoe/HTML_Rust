1d2210fce9634f83541fb528d858c9a84dd30652
Steven Shoemaker <stshoema@ucsd.edu>
2023-02-02 18:08:57 -0800

    Began basic work and datatypes before swapping over to a GO build

---
+++ rusty_app/src/main.rs
@@ -0,0 +1,42 @@
+#![allow(non_snake_case)]
+
+static BINARYARRAY : [u8; 8] = [128, 64, 32, 16, 8, 4, 2, 1];
+
+struct SDR {
+    composition: [MicroSDR]
+}
+
+enum MicroSDR {
+    Quantitative {n: usize, w: usize, mix: usize, max: usize, periodic: bool},
+    QuantitativeRDSE,
+    Qualitative {x: u8},
+}
+
+fn main() {
+    let mut x : [u8; 4] = [0, 1, 2, 31];
+
+    flipNthBit(1, &mut x);
+
+    printSDR(&x);
+}
+
+fn flipNthBit(index : usize, sdr : &mut [u8]) {
+
+    let sdrIdx = index / 8;
+    let subIdx = index % 8;
+
+    sdr[sdrIdx] = sdr[sdrIdx] ^ BINARYARRAY[subIdx];
+}
+
+fn printSDR(sdr : &[u8]) {
+
+    println!("----------");
+
+    for i in 0..4
+    {
+        let j = sdr[i];
+        println!("|{}|", format!("{:0>8}", format!("{j:b}") ) );
+    }
+
+    println!("----------");
+}
---
+++ rusty_app/target/debug/deps/rusty_app-ada31e4dada3b2de.d
@@ -0,0 +1,5 @@
+c:\Users\skill\Desktop\HTM_Rust\rusty_app\target\debug\deps\rusty_app-ada31e4dada3b2de.rmeta: src\main.rs
+
+c:\Users\skill\Desktop\HTM_Rust\rusty_app\target\debug\deps\rusty_app-ada31e4dada3b2de.d: src\main.rs
+
+src\main.rs:
